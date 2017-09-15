f82bc2e2ac9d7a2a875a31965f78abafcceaa3fe
Helmut Hummel <info@helhum.io>
2017-09-15 18:01:37 +0200

    Check whether this ever worked with TYPO3 7.6

--- Tests/Functional/Fixtures/Frontend/Basic.ts
+++ Tests/Functional/Fixtures/Frontend/Basic.ts
@@ -30,7 +30,6 @@ lib.viewConfig {
 		1 = EXT:fluid_test/Resources/Private/Base/Layouts/
 	}
 	widget.TYPO3\CMS\Fluid\ViewHelpers\Widget\PaginateViewHelper {
-		templateRootPath = EXT:fluid_test/Resources/Private/Base/Templates/
 		templateRootPaths {
 			1 = EXT:fluid_test/Resources/Private/Base/Templates/
 		}
